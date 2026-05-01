404f82e9828b32af6b23466cd4733b0b9fd48a73
Varun Yadav <yadavvarunrai2002@gmail.com>
2026-05-01 16:29:15 +0530

    test routes issue

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,10 @@ app.get("/", (req, res) => {
 });
 
 // routes
-app.use("/api/auth", authRoutes);
-app.use("/api/projects", projectRoutes);
-app.use("/api/tasks", taskRoutes);
-app.use("/api/users", userRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/projects", projectRoutes);
+// app.use("/api/tasks", taskRoutes);
+// app.use("/api/users", userRoutes);
 
 // 🔥 SERVER FIRST START
 const PORT = process.env.PORT || 8000;
